f8a8bb6128c26d6085c8c64e4d840cf6f9943339
Luke Novak <luken22@optonline.net>
2019-04-23 20:00:28 -0400

    restructured repo

--- scrapers/discogs-scrape.py
+++ scrapers/discogs-scrape.py
@@ -3,16 +3,16 @@ import psycopg2
 import time
 
 # create the db connection
-dbconn = psycopg2.connect(host='', dbname='',
-                          user='', password=''`)
+dbconn = psycopg2.connect(host='fernando.nluken.com', dbname='music_analysis',
+                          user='nluken', password='arcadia22')
 cur = dbconn.cursor()
 cur2 = dbconn.cursor()
 print("connected to db\n")
 
-d = discogs_client.Client('DiscogsFormatDataAnalyzer/0.1', user_token='insert token here')
+d = discogs_client.Client('DiscogsFormatDataAnalyzer/0.1', user_token='gAHNhkitFMXWlVoRKZGVZpgpDojaDVznMDvBYJNt')
 
 #get the albums table
-cur.execute("SELECT * FROM albums WHERE genre IS NULL;")
+cur.execute("SELECT * FROM albums WHERE genre IS NULL order by title desc;")
 for album in cur:
   album_info=d.search(album[1], artist=album[2], type='master')
   print("getting album " + album[1], ", with id " + album[0])
@@ -24,6 +24,9 @@ for album in cur:
     cur2.execute("UPDATE albums SET genre = %s WHERE id = %s", (album_genre, album[0]))
     dbconn.commit()
   ## we need to sleep to avoid rate limits
+  else:
+    cur2.execute("UPDATE albums SET genre = 'None' WHERE id = '%s'" % album[0])
+    dbconn.commit()
   time.sleep(2)
   
 
